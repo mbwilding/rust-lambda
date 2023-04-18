04384d6c9f7eeb66614b30f35cdac86c42350c33
mbwilding <mbwilding@gmail.com>
2023-04-19 03:10:57 +0800

    Testing S3

---
+++ Setup.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco feature enable -n=allowGlobalConfirmation
+choco install mingw
+
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+irm get.scoop.sh | iex
+scoop bucket add cargo-lambda https://github.com/cargo-lambda/scoop-cargo-lambda
+scoop install cargo-lambda/cargo-lambda
+
+[Environment]::SetEnvironmentVariable("TARGET_CC", "C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin\gcc.exe", "Machine")
+[Environment]::SetEnvironmentVariable("TARGET_AR", "C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin\ar.exe", "Machine")
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 /// The runtime pays no attention to the contents of the request payload.
 #[derive(Deserialize)]
 struct Request {
+    #[serde(alias = "Command")]
     command: String,
 }
 
@@ -34,6 +35,13 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
         msg: format!("Command: {}.", command),
     };
 
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_s3::Client::new(&config);
+    let bucket_resp = get_buckets(&client).await?;
+    for bucket in bucket_resp.into_iter() {
+        _ = list_objects(&client, &bucket).await?;
+    }
+
     // Return `Response` (it will be serialized to JSON automatically by the runtime)
     Ok(resp)
 }
@@ -49,4 +57,27 @@ async fn main() -> Result<(), Error> {
         .init();
 
     run(service_fn(function_handler)).await
+}
+
+async fn get_buckets(client: &aws_sdk_s3::Client) -> Result<Vec<String>, Error> {
+    let resp = client.list_buckets().send().await?;
+    let buckets = resp.buckets().unwrap_or_default();
+
+    let bucket_names: Vec<String> = buckets
+        .iter()
+        .map(|bucket| bucket.name().unwrap_or_default().to_string())
+        .collect();
+
+    Ok(bucket_names)
+}
+
+pub async fn list_objects(client: &aws_sdk_s3::Client, bucket_name: &str) -> Result<(), Error> {
+    let objects = client.list_objects_v2().bucket(bucket_name).send().await?;
+    println!("Objects in bucket: {}", bucket_name);
+    let files = objects.contents().unwrap_or_default();
+    for file in files {
+        println!("{:?}", file.key().unwrap());
+    }
+
+    Ok(())
 }
